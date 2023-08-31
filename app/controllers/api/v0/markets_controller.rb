2acb9e9efb09cdcd3303e59ee3f07ce0be7d1cd6
Jeff Nelson <jpnelson85@gmail.com>
2023-08-31 15:59:01 -0500

    add error tests

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -3,11 +3,25 @@ class Api::V0::MarketsController < ApplicationController
     render json: MarketSerializer.new(Market.all)
   end
 
+  # def show
+  #   if Market.find(params[:id]).exists?
+  #     render json: MarketSerializer.new(Market.find(params[:id]))
+  #   else
+  #     render json: { message: "Market not found", error: "string", body: "string" } status: :not_found
+  #   end
+  # end
+
   def show
-    if 
+    begin
       render json: MarketSerializer.new(Market.find(params[:id]))
-    else 
-      render json: ErrorMemberSerializer.new(Market.find(params[:id]).serialized_json)
+    rescue ActiveRecord::RecordNotFound => error
+      render json: {
+        errors: [
+          {
+            detail: "Couldn't find Market with 'id'=#{params[:id]}"
+          }
+        ]
+      }, status: 404
     end
   end
 
--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,16 +1,45 @@
 class Api::V0::VendorsController < ApplicationController
   def index
-    render json: VendorSerializer.new(Vendor.all)
+    begin
+      render json: VendorSerializer.new(Market.find(params[:market_id]).vendors, status: 200)
+    rescue ActiveRecord::RecordNotFound => error
+      render json: {
+        errors: [
+          {
+            detail: "Couldn't find Market with 'id'=#{params[:market_id]}"
+          }
+        ]
+      }, status: 404
+    end
   end
 
   def show
-    render json: VendorSerializer.new(Vendor.find(params[:id]))
+    begin
+      render json: VendorSerializer.new(Vendor.find(params[:id]))
+    rescue ActiveRecord::RecordNotFound => error
+      render json: {
+        errors: [
+          {
+            detail: "Couldn't find Vendor with 'id'=#{params[:id]}"
+          }
+        ]
+      }, status: 404
+    end
   end
 
   def create
-    @vendor = Vendor.create(vendor_params)
-    @vendor.save
-    render json: VendorSerializer.new(@vendor)
+    begin  
+      vendor = Vendor.create!(vendor_params)
+      render json: VendorSerializer.new(Vendor.last), status: 201
+    rescue ActiveRecord::RecordInvalid => error
+      render json: {
+        errors: [
+          {
+            detail: "Validation failed: Contact name can't be blank, Contact phone can't be blank"
+          }
+        ]
+      }, status: 400
+    end
   end
 
   def update
@@ -27,6 +56,6 @@ class Api::V0::VendorsController < ApplicationController
   private
 
   def vendor_params
-    params.permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
+    params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
   end
 end
